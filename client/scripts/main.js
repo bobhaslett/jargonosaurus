fac33f6d60a7fdb345bec9609e3c0630834a27e9
Bob Haslett <bob.haslett@ft.com>
2015-11-06 12:29:55 +0000

    change to date function and related words button
    Luke created an index used to add data to page and create working word
    buttons. Also created proper date formats

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,6 +1,10 @@
 import oHoverable from 'o-hoverable';
 import FastClick from 'fastclick';
 
+function is_valid_date(date) {
+	return date && date instanceof Date && !isNaN(date.getTime());
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	// make hover effects work on touch devices
 	oHoverable.init();
@@ -9,23 +13,49 @@ document.addEventListener('DOMContentLoaded', () => {
 	FastClick.attach(document.body);
 
 	// YOUR CODE HERE!
-	var dataset=spreadsheet.data
+	var dataset = spreadsheet.data.map(function(row) {
+		row.submisiondate = new Date(row.submisiondate);
+		if (!is_valid_date(row.submissiondate)) {
+			row.submisiondate = new Date(null); // Epoch.
+		}
+		return row;
+	});
+	
+	var index_by_word = dataset.reduce(function(ind, row){
+		if (ind[row.word]) {
+			ind[row.word.toLowerCase()].push(row)
+		} else if (row.word) {
+			ind[row.word.toLowerCase()] = [row];
+		}
+		return ind;
+	}, {});
+
+	console.dir(index_by_word)
+
 	// console.log("dataset",dataset);
 	var width= document.getElementById('holder').getBoundingClientRect().width;
 	window.addEventListener('resize', resize);
 	var divSelect = document.getElementById('lucyLogo');
 	divSelect.addEventListener("click",latest);
 	divSelect = document.getElementById('searchText');
-	divSelect.addEventListener("change",search);
-
+	divSelect.addEventListener("keyup",search);
 	latest ();
 
 	function search() {
-		var lookup=String(this.value)
-		console.log("search",lookup);
-		var results=dataset.filter(function(el){
-			return (el.word.indexOf(lookup) > -1)
-		});
+
+		if (!this.value) {
+			setTimeout(latest, 0);
+			return;
+		}
+
+		var search_term = (this.value).toLowerCase();
+
+		var results = Object.keys(index_by_word).filter(function(word){
+			return word.indexOf(search_term) === 0;
+		}).reduce(function(arr, word) {
+			return arr.concat(index_by_word[word]);
+		}, []);
+
 		if((results.length)<1){
 			var divSelect = document.getElementById('jargon');
 			divSelect.innerHTML='<div class="slide_defin">Sorry there where no matches</div>';
@@ -39,19 +69,19 @@ document.addEventListener('DOMContentLoaded', () => {
 	function latest () {
 		console.log("latest")
 		dataset=sortDS(dataset);
-		var mostRecent=10;
+		var mostRecent=19;
 	  	var divSelect = document.getElementById('buttonHolder');
 	  	divSelect.innerHTML='<button id="submit" class="o-buttons o-buttons--standout">Submit your jargon here</button>'
 	  	var button = document.getElementById('submit');
 	  	button.addEventListener("click",submit);
 	  	var divSelect = document.getElementById('jargon');
 	  	divSelect.innerHTML='<div class="slide_defin">Most recent submissons (click word for the full definition, Lucy’s comments and related words)</div></p>';
-		buildDef(data)for (var i = 0; i < mostRecent; i++) {
+		for (var i = 0; i < mostRecent; i++) {
 			var wordContent=''
 			var annotation=dataset[i]
 			var str =String(annotation.commenturl)
 			wordContent=wordContent+'<div class="slide_date">'+annotation.submisiondate+'</div>'+
-			'<div class="slide_word" id="'+annotation.wordid+'">'+annotation.word+'</div>'+
+			'<div class="slide_word" data-word="'+ annotation.word.toLowerCase() +'" id="'+annotation.wordid+'">'+annotation.word+'</div>'+
 			'<div class="slide_type">'+annotation.wordtype+'</div>'+
 			'<div class="slide_url">Seen at: '+convertlink(str)+'</div></p>'+
 			'<div class="slide_subhead">Plain english definition</div>'+
@@ -76,23 +106,40 @@ document.addEventListener('DOMContentLoaded', () => {
 			var annotation=data[i]
 			var str =String(annotation.commenturl)
 			wordContent=wordContent+'<div class="slide_date">'+annotation.submisiondate+'</div>'+
-			'<div class="slide_word" id="'+annotation.wordid+'">'+annotation.word+'</div>'+
+			'<div class="slide_word" data-word="'+ annotation.word.toLowerCase() +'" id="'+annotation.wordid+'">'+annotation.word+'</div>'+
 			'<div class="slide_type">'+annotation.wordtype+'</div>'+
 			'<div class="slide_url">Seen at: '+convertlink(str)+'</div></p>'+
 			'<div class="slide_subhead">Plain english definition</div>'+
 			'<div class="slide_defin">'+annotation.definition+'</div></p>'+
-			'<div class="slide_subhead">Lucy’s commentary</div>'+
-			'<div class="slide_defin">'+annotation.lucycommentary+'</div></p>'+
 			'<div class="slide_subhead">Usage example</div>'+
 			'<div class="slide_defin">'+annotation.usageexample+'</div></p>'+
-			'<div class="slide_type">Related words</div>'+
-			'<div class="slide_wordSmall">'+annotation.relatedwords+'</div>'+
-			'<div class="slide_bottom"></div></p></p>'
+			'<div class="slide_subhead">Lucy’s commentary</div>'+
+			'<div class="slide_defin">'+annotation.lucycommentary+'</div></p>'+
+			'<div class="slide_type">Related words</div>'
+			
+			console.log("annotations is",annotation.relatedwords);
+			if (annotation.relatedwords!== null) {
+				var split=annotation.relatedwords;
+				var related='';
+				for (var j = 0; j < split.length; j++) {
+					//console.log("split ",split[j]);
+					var newID=dataset.filter(function(el){return el.word===split[j]});
+					//console.log("newID ",newID[0].wordid)
+					related=related+'<div class="slide_wordSmall" data-word="'+ split[j].toLowerCase() +'" id="'+annotation.wordid+'">'+split[j]+',</div>'
+				}
+			}
+			else {
+				var related='';
+			}
+			wordContent=wordContent+related+'<div class="slide_bottom"></div></p></p>'
 			divSelect.innerHTML=divSelect.innerHTML+wordContent;
-
 		}
-
+		select('.slide_wordSmall').forEach(function (el) {
+				el.addEventListener("click",fullDef);
+			})
 	}
+	
+		
 
 	function select(selector, parent=document) {
 			 // return an array of elements matching the selector
@@ -100,12 +147,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	function fullDef() {
-		var lookup=this.id
-		var results=dataset.filter(function(el){
-			return el.wordid==lookup
-		});
-		console.log("Results ",results);
-		buildDef (results)
+		var word = this.getAttribute('data-word');
+		var definitions = index_by_word[word];
+		console.log(word)
+		console.dir(definitions)
+		//console.log("Results ",results);
+		buildDef (definitions)
 	}
 
 	function submit() {
